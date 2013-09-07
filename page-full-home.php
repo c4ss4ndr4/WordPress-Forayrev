fd11f6d0047152277b8a3bceb17baa319ef6fd07
c4ss4ndr4 <cassandra.marshall@gmail.com>
2013-09-07 08:10:22 -0700

    Foray Child Theme Based on Reverie

---
+++ page-full-home.php
@@ -0,0 +1,30 @@
+<?php
+/*
+Template Name: Full Width Foray Home
+*/
+get_header(); ?>
+
+<!-- Row for main content area -->
+	<div class="small-12 large-12 columns content" role="main">
+	<div class="contentInner">
+	<?php /* Start loop */ ?>
+	<?php while (have_posts()) : the_post(); ?>
+		<!-- <article <?php post_class() ?> id="post-<?php the_ID(); ?>">
+			<header>
+				<h1 class="entry-title"><?php //the_title(); ?></h1>
+				<?php //reverie_entry_meta(); ?>
+			</header> -->
+			<div class="entry-content">
+				<?php the_content(); ?>
+			</div>
+			<footer>
+				<?php wp_link_pages(array('before' => '<nav id="page-nav"><p>' . __('Pages:', 'reverie'), 'after' => '</p></nav>' )); ?>
+				<p><?php the_tags(); ?></p>
+			</footer>
+			<?php //comments_template(); ?>
+		</article>
+	<?php endwhile; // End the loop ?>
+	</div> <!-- contentInner -->
+	</div>
+		
+<?php get_footer(); ?>
